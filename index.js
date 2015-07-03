af4e4fd193995e307c5a589e811ae5ea8b9772a7
Marty Zalega <marty@zalega.me>
2015-07-03 10:28:00 +1000

    Add support for debug mode

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ var s3 = new AWS.S3({
   secretAccessKey: process.env.AWS_SECRET
 });
 
-var importer = require("./importer")(s3);
+var elasticsearchUrl = process.env.ELASTICSEARCH_URL;
+var s3Bucket = process.env.S3_BUCKET;
+var debug = process.env.DEBUG;
+var importer = require("./importer")(s3, {debug: debug});
 
-if (!process.env.ELASTICSEARCH_URL) {
+if (!elasticsearchUrl) {
   console.log("You must specify ELASTICSEARCH_URL");
   process.exit(1);
 }
 
-if (process.env.S3_BUCKET) {
-  importer.importFolder(process.env.S3_BUCKET, process.env.ELASTICSEARCH_URL).then(function(data) {
-    console.log(data);
+if (s3Bucket) {
+  importer.importFolder(s3Bucket, elasticsearchUrl).then(function(data) {
+    console.log("Finished importing from bucket", s3Bucket, "to", elasticsearchUrl);
+    console.log("Summary:", data);
   }, function(error) {
     if (typeof(error) === "object" && error.message) {
       console.error(error.message);
